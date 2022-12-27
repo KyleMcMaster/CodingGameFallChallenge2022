e30bdccf8ac837ed1eebd862ef6598b2b62b933f
Kyle McMaster <KyleMcMaster@users.noreply.github.com>
2022-12-27 14:07:34 -0500

    init

--- Program.cs
+++ Program.cs
@@ -1,2 +1,61 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace CodingGameFallChallenge2022;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+class Player
+{
+    class GameInputs
+    {
+        public int ScrapAmount { get; set; }        
+        public int Owner { get; set; }
+        public int Units { get; set; }
+        public int Recycler { get; set; }        
+        public int CanBuild { get; set; }
+        public int CanSpawn { get; set; }
+        public int InRangeOfRecycler { get; set; }        
+    }
+
+    static void Main(string[] args)
+    {
+        string[] inputs;
+        inputs = Console.ReadLine().Split(' ');
+        int width = int.Parse(inputs[0]);
+        int height = int.Parse(inputs[1]);
+
+        // game loop
+        while (true)
+        {
+            inputs = Console.ReadLine().Split(' ');
+            int myMatter = int.Parse(inputs[0]);
+            int oppMatter = int.Parse(inputs[1]);
+            for (int i = 0; i < height; i++)
+            {
+                for (int j = 0; j < width; j++)
+                {
+                    inputs = Console.ReadLine().Split(' ');
+                    int scrapAmount = int.Parse(inputs[0]);
+                    int owner = int.Parse(inputs[1]); // 1 = me, 0 = foe, -1 = neutral
+                    int units = int.Parse(inputs[2]);
+                    int recycler = int.Parse(inputs[3]);
+                    int canBuild = int.Parse(inputs[4]);
+                    int canSpawn = int.Parse(inputs[5]);
+                    int inRangeOfRecycler = int.Parse(inputs[6]);
+                }
+            }
+
+            // Write an action using Console.WriteLine()
+            // To debug: Console.Error.WriteLine("Debug messages...");
+
+            Console.WriteLine("MOVE 1");
+        }
+    }
+}
